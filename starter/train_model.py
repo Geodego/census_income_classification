68df50876d050280b5a0e5337cdea2fb45cc2de0
Geodego <geoffroydegournay@hotmail.com>
2022-04-12 10:15:52 +0200

    feat: Modification of train_model.py

--- starter/train_model.py
+++ starter/train_model.py
@@ -3,8 +3,10 @@
 from sklearn.model_selection import train_test_split
 
 # Add the necessary imports for the starter code.
+from .ml.data import process_data, get_raw_data
 
 # Add code to load in the data.
+data = get_raw_data()
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
@@ -23,6 +25,8 @@ X_train, y_train, encoder, lb = process_data(
     train, categorical_features=cat_features, label="salary", training=True
 )
 
-# Proces the test data with the process_data function.
+# Process the test data with the process_data function.
+X_test, y_test, _, _ = process_data(test, categorical_features=cat_features, label="salary",
+                                    training=False, encoder=encoder, lb=lb)
 
 # Train and save a model.
