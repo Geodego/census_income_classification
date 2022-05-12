d3fa55ab0e078ff09a31d6b066e528ecd7e2ec6c
Geodego <geoffroydegournay@hotmail.com>
2022-05-12 15:03:51 +0200

    test: tests added

--- starter/tests/test_ml.py
+++ starter/tests/test_ml.py
@@ -1,28 +1,35 @@
 import pytest
-from ..ml.data import get_raw_data, get_path_root
+import numpy as np
+from torch import nn
+from ..ml.nn_model import build_mlp
+from ..ml.nn_model import Mlp
 
 
-@pytest.fixture(scope='module')
-def raw_data():
+@pytest.mark.parametrize('n_layers', [1, 2])
+def test_build_mlp(n_layers):
     """
-    Get raw data and return a pd.DataFrame
+    Test if the neural network is built properly
     """
-    df = get_raw_data()
-    return df
+    input_size = 10
+    output_size = 2
+    hidden_size = 5
+    model = build_mlp(input_size, output_size, n_layers, hidden_size, dropout_rate=0.5)
+    assert len(model) == 3 * (n_layers+1)
+
+    assert type(model[0]) is nn.Linear
+    assert model[0].in_features == input_size
+    assert model[0].out_features == hidden_size
+    assert type(model[1]) == nn.ReLU
+    assert type(model[3]) == nn.Dropout
+    assert model[-1].out_features == output_size
 
 
-def test_get_path_root():
+def test_mlp_inference():
     """
-    Test that path returned is the absolut path to the project directory
+    Test if the model returns expected output when used for inference
     """
-    path_root = get_path_root()
-    assert path_root.name == 'census_income_classification'
-    assert path_root.is_absolute()
-
-
-def test_get_raw_data(raw_data):
-    """
-    Test if the raw data we get are as expected
-    """
-    assert not raw_data.empty
-    assert len(raw_data.columns) == 15
+    model = Mlp(n_layers=2, hidden_dim=5,n_classes=2, input_dim=10)
+    n_examples =300
+    data = np.random.rand(n_examples, 10)
+    output = model.predict(data)
+    assert output.shape[0] == n_examples
