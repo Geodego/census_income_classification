d74f4f99122f6d3f569b7430878455c6378fcfaa
Geodego <geoffroydegournay@hotmail.com>
2022-05-12 14:48:19 +0200

    test: Creation of test_data.py

---
+++ starter/tests/test_data.py
@@ -0,0 +1,48 @@
+import pytest
+from ..ml.data import get_raw_data, get_path_root, get_hyperparameters, get_cat_features
+
+
+@pytest.fixture(scope='module')
+def raw_data():
+    """
+    Get raw data and return a pd.DataFrame
+    """
+    df = get_raw_data()
+    return df
+
+
+def test_get_path_root():
+    """
+    Test that path returned is the absolut path to the project directory
+    """
+    path_root = get_path_root()
+    assert path_root.name == 'census_income_classification'
+    assert path_root.is_absolute()
+
+
+def test_get_raw_data(raw_data):
+    """
+    Test if the raw data we get are as expected
+    """
+    assert not raw_data.empty
+    assert len(raw_data.columns) == 15
+
+
+def test_get_hyperparameters():
+    params = get_hyperparameters()['parameters']
+    assert set(params.keys()) == {'batch_size', 'dropout_rate', 'hidden_dim', 'learning_rate', 'n_layers'}
+
+
+def test_get_cat_features():
+    exp_cat_features = [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country",
+    ]
+    cat_features = get_cat_features()
+    assert cat_features == exp_cat_features
