7254ee275b7505da96041116d1681987a69f1d9f
Geodego <geoffroydegournay@hotmail.com>
2022-05-18 12:13:26 +0200

    tests: Reorganise tests to make them pass sanitycheck.py

--- starter/tests/test_api.py
+++ starter/tests/test_api.py
@@ -29,7 +29,8 @@ def test_api_get_root(client):
     r = client.get("/")
     assert r.status_code == 200
     output = r.json()
-    assert 'greeting' in output
+    expected_output = {'greeting': 'Welcome! This API predicts income category using Census data.'}
+    assert output == expected_output
 
 
 @pytest.fixture(scope='class')
@@ -40,31 +41,51 @@ def predict_request(client, positive_example, negative_example):
     return requests
 
 
-class TestAPIPredict:
-    """
-    Tests for API post predict
-    """
+# Tests for API post predict
 
-    def test_api_predict_basic(self, predict_request):
-        response = predict_request['negative']
-        assert response.status_code == 200
-        output = response.json()['predicted_salary_class']
-        assert type(output) == int
 
-    def test_api_predict(self, predict_request, positive_example, negative_example):
-        """
-        Test that the prediction we get from using the api is identical to the one we get from inferring
-        directly from the model. We consider both an example where the model predicts a positive outcome and a case
-        where the model predicts a negative outcome.
-        """
-        for type, example in zip(['positive', 'negative'], [positive_example, negative_example]):
-            response = predict_request[type]
-            output = response.json()['predicted_salary_class']
-            data = pd.DataFrame([example])
-            cat_features = get_cat_features()
-            model = get_trained_mlp()
-            x, _, _, _, _ = process_data(data, categorical_features=cat_features, label="salary",
-                                         training=False, encoder=model.encoder, lb=model.lb, scaler=model.scaler)
-            predicted = inference(model, x)
-            expected_output = predicted[0]
-            assert output == expected_output, f"API prediction failed for an example labelled as {type} by the model"
+def test_api_post_basic(predict_request):
+    response = predict_request['negative']
+    assert response.status_code == 200
+    output = response.json()['predicted_salary_class']
+    assert type(output) == int
+
+
+def test_api_post_positive(positive_example, client):
+    """
+    Test that the prediction we get from using the api is identical to the one we get from inferring
+    directly from the model. We consider both an example where the model predicts a positive outcome and a case
+    where the model predicts a negative outcome.
+    """
+    type_ex, example = 'positive', positive_example
+    response = client.post("/predict/", json=positive_example)
+    output = response.json()['predicted_salary_class']
+    data = pd.DataFrame([example])
+    cat_features = get_cat_features()
+    model = get_trained_mlp()
+    x, _, _, _, _ = process_data(data, categorical_features=cat_features, label="salary",
+                                 training=False, encoder=model.encoder, lb=model.lb, scaler=model.scaler)
+    predicted = inference(model, x)
+    expected_output = predicted[0]
+    assert response.status_code == 200
+    assert output == expected_output, f"API prediction failed for an example labelled as {type_ex} by the model"
+
+
+def test_api_post_negative(predict_request, negative_example, client):
+    """
+    Test that the prediction we get from using the api is identical to the one we get from inferring
+    directly from the model. We consider both an example where the model predicts a positive outcome and a case
+    where the model predicts a negative outcome.
+    """
+    type_ex, example = 'negative', negative_example
+    response = client.post("/predict/", json=negative_example)
+    output = response.json()['predicted_salary_class']
+    data = pd.DataFrame([example])
+    cat_features = get_cat_features()
+    model = get_trained_mlp()
+    x, _, _, _, _ = process_data(data, categorical_features=cat_features, label="salary",
+                                 training=False, encoder=model.encoder, lb=model.lb, scaler=model.scaler)
+    predicted = inference(model, x)
+    expected_output = predicted[0]
+    assert response.status_code == 200
+    assert output == expected_output, f"API prediction failed for an example labelled as {type_ex} by the model"
